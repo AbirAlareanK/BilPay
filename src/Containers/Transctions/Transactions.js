1a58bc1e4ac41ef0977366ec6006915cb0d925d7
Abir Alarean <abir.alarean27me@gmail.com>
2021-12-12 19:34:49 +0800

    Style transaction component

--- src/Containers/Transctions/Transactions.js
+++ src/Containers/Transctions/Transactions.js
@@ -3,26 +3,43 @@ import { Col , Row , Container } from "react-bootstrap";
 import Classes from './Transactions.module.scss';
 import AdminCard from "../../Components/UIs/AdminCard";
 import Cards from '../../Assets/JSON/cards-data.json';
-import Services from '../../Assets/JSON/services.json';
 import Invoices from '../../Assets/JSON/invoices-data.json';
+import { useState } from "react";
 
 const Transactions = () => {
 
     const cardData = Cards[0]
+    const [ services ] = useState([
+        {
+            id : 'SER_001',
+            serviceName : 'Web Design and Development',
+            servicePrice : '750.00'
+        },
+        {
+            id : 'SER_002',
+            serviceName : 'Hosting',
+            servicePrice : '50.00'
+        }
+    ]);
+
+    const [ tax ] = useState('10.00');
 
     return(
         <Container fluid>
             <Row>
                 <h4>Transaction Details</h4>
+            </Row>
+            <Row>
                 <div className={Classes.heading}>
                     <h6>Transaction</h6>
-                    <p>/{}</p>
+                    <p>/{Invoices[0]['invoice-number']}</p>
                 </div>
             </Row>
             <Row>
                 <Col lg={8}>
-                    <Transaction Service={Services[0]}
-                                 invoice={Invoices[0]}   />
+                    <Transaction services={services}
+                                 invoice={Invoices[0]} 
+                                 tax={tax}  />
                 </Col>  
                 <Col lg={4}>
                     <div className={Classes.CardUsed}>
