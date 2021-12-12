ee3a230009c2e9086c1d8ef807f7aa8daa90e129
Abir Alarean <abir.alarean27me@gmail.com>
2021-12-12 19:34:49 +0800

    Style transaction component

--- src/Components/Transactions/Transaction.js
+++ src/Components/Transactions/Transaction.js
@@ -1,79 +1,71 @@
-import { useState } from 'react';
 import { BsPrinter , BsDownload} from 'react-icons/bs';
 import Button from '../../Components/UIs/Button';
 import Classes from './Transaction.module.scss';
 
 
 const Transaction = (props) => {
-    const { invoice } = props
-    const [ services ] = useState([
-        {
-            id : 'SER_001',
-            serviceName : 'Web Design and Development',
-            servicePrice : '750.00'
-        },
-        {
-            id : 'SER_002',
-            serviceName : 'Hosting',
-            servicePrice : '50.00'
-        }
-    ]);
-
-    const [ tax ] = useState('10.00');
+    const { services , tax ,  invoice } = props;
 
     return (
         <div className="card-wrapper">
             <section>
-                <h4>{invoice['invoice-number']}</h4>
-                <p>{invoice['invoice-date']}</p>
-                <div>
-                    <Button>
-                        <BsPrinter />
-                        Print
-                    </Button>
-                    <Button>
-                        <BsDownload />
-                        Download
-                    </Button>
+                <div className={Classes.tranheading}>
+                    <div>
+                        <h5>#{invoice['invoice-number']}</h5>
+                        <p>{invoice['invoice-date']}</p>
+                    </div>
+                    <div className={Classes.actionButtons}>
+                        <Button className={Classes.transButton}>
+                            <BsPrinter />
+                            Print
+                        </Button>
+                        <Button className={Classes.transButton}>
+                            <BsDownload />
+                            Download
+                        </Button>
+                    </div>
                 </div>
                 <div className={Classes.clientDetails}>
                     <div>
-                        <h5>Client</h5>
-                        <p>{invoice['client']}</p>
+                        <p>Client</p>
+                        <h6>{invoice['client']}</h6>
                     </div>
                     <div>
-                        <h5>Company</h5>
-                        <p>{invoice['company-name']}</p>
+                        <p>Company</p>
+                        <h6>{invoice['company-name']}</h6>
                     </div>
                     <div>
-                        <h5>Payment Type</h5>
-                        <p>Paypal</p>
+                        <p>Payment Type</p>
+                        <h6>Paypal</h6>
                     </div>
                 </div>
-                <div className={Classes.serviceSection}>
-                    <h5>Service</h5>
-                    {services.map(service => (
-                        <div key={service.id}>
-                            <h5>{service.serviceName}</h5>
-                            <p>{`$${service.servicePrice}`}</p>
+                <div className={Classes.darksection} style={{marginBottom:'15px'}}>
+                    <h6 className={Classes.bold} style={{paddingBottom: '10px'}}>Service</h6>
+                    <section className={Classes.devidedline}>
+                        {services.map(service => (
+                            <div key={service.id} className={Classes.linedsection}>
+                                <h6>{service.serviceName}</h6>
+                                <p>{`$${service.servicePrice}`}</p>
+                            </div>
+                        ))}
+                    </section>
+                    <section>
+                        <div className={Classes.linedsection}>
+                            <h6>Subtotal</h6>
+                            <p>$800.00</p>
                         </div>
-                    ))}
-                    <span className={Classes.deviderline}/>
-                    <div>
-                        <h5>Subtotal</h5>
-                        <p>$800.00</p>
-                    </div>
-                    <div>
-                        <h5>Tax</h5>
-                        <p>{`$${tax}`}</p>
-                    </div>
-                    <div>
-                        <h5>Total</h5>
+                        <div className={Classes.linedsection}>
+                            <h6>Tax</h6>
+                            <p>{`$${tax}`}</p>
+                        </div>
+                    </section>
+                    <div className={Classes.linedsection}>
+                        <h6  className={Classes.bold}>Total</h6>
                         <p>$810.00</p>
                     </div>
                 </div>
-                <div className={Classes.note}>
-                    <h5>Note:</h5>
+                <div className={Classes.darksection}>
+                    <h6 style={{paddingBottom: '10px'}} className={Classes.bold}>Note:</h6>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
             </section>
