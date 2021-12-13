acef39cb219283ed838ba1621e0f742e98e9d5a2
Abir Alarean <abir.alarean27me@gmail.com>
2021-12-13 18:25:55 +0800

    resolve rebase conflict

--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import App from './App';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
+import { BrowserRouter} from "react-router-dom";
 
 
 ReactDOM.render(
-      <App />,
+  <BrowserRouter>
+        <App/>
+  </BrowserRouter>,
   document.getElementById('root')
 );
