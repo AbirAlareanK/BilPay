3d71e020aa7d1ecf167aaf37316e52282c13b34c
Abir Alarean <abir.alarean27me@gmail.com>
2021-12-28 14:18:32 +0800

    Notification menu

---
+++ src/Components/Notifications/NotificationMenu.js
@@ -0,0 +1,28 @@
+import Notifications from "react-notifications-menu"
+import bell from '../../Assets/imgs/bell.png'
+import user from '../../Assets/imgs/user.png'
+
+const NotificationMenu = (props) => {
+    const data = [
+        {
+            image: user,
+            message: 'Kameshwaran S had shared a feedback with you.',
+            // detailPage: '/',
+        },
+        {
+            image: user,
+            message: 'Kameshwaran S had shared a feedback with you.',
+            // detailPage: '/',
+        }
+    ];
+
+
+    return (
+        <Notifications
+            data={data}
+            icon={bell}
+        />
+    );
+};
+
+export default NotificationMenu;
